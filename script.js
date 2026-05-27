21869d2d0c84d29660aab021cfcbf3a45071177d
Diego M. <diego213093gma@gmail.com>
2026-05-26 21:05:23 -0300

    botão de tema

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-// Aguarda o documento ser carregado
+// Aguarda o documento ser totalmente carregado
 document.addEventListener("DOMContentLoaded", function() {
     
-    // id de formulário
+
     const formContato = document.getElementById("form-contato");
 
-    // faz o envio do formulário
     formContato.addEventListener("submit", function(event) {
         
-        // carregar a página sem carregar de novo
         event.preventDefault(); 
 
-        // valores do formulário
         const nome = document.getElementById("nome").value.trim();
         const email = document.getElementById("email").value.trim();
         const mensagem = document.getElementById("mensagem").value.trim();
 
-        // 1. Validação de campos
+        // 1. validar de campos vazios
         if (nome === "" || email === "" || mensagem === "") {
             alert("Atenção: Por favor, preencha todos os campos antes de enviar.");
-            return; // Interrompe a execução da função
+            return; 
         }
 
-        // 2. Validação de e-mail
+        // 2. validar formato de e-mail
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
             alert("Atenção: Por favor, insira um endereço de e-mail válido.");
             return;
         }
 
-        // 3. simular envio
+        // 3. envio com sucesso
         alert("Mensagem enviada com sucesso!\n\nObrigado pelo contato, " + nome + ". Responderei em breve.");
 
-        // 4. Limpezado formulário
+        // 4. limpeza dos campos
         formContato.reset();
     });
+
+    // tema escuro(dark)
+    const btnTema = document.getElementById("btn-tema");
+
+    btnTema.addEventListener("click", function() {
+        document.body.classList.toggle("dark-theme");
+
+        // mudar cor do botão de tema
+        if (document.body.classList.contains("dark-theme")) {
+            btnTema.textContent = "☀️ Tema Claro";
+        } else {
+            btnTema.textContent = "🌙 Tema Escuro";
+        }
+    });
+
 });
